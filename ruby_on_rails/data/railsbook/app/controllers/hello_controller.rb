aa12c379c430a0a79850a78e006fa39ac2bd3390
yoshikazu HIRAI <hirai.yoshikazu@adways.net>
2015-06-17 00:23:33 +0900

    Chapter 2 Basis of Ruby on Rails
    hello world!
    making template file

---
+++ ruby_on_rails/data/railsbook/app/controllers/hello_controller.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+class HelloController < ApplicationController
+  def index
+    render text: 'こんにちは、世界！'
+  end
+
+  def view
+    @msg = 'こんにちは、世界！'
+    render 'hello/view'
+  end
+end
