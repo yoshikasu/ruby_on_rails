42b368d950fbc9eb337878d37bcb5e31bea06209
yoshikazu HIRAI <hirai.yoshikazu@adways.net>
2015-06-18 00:11:13 +0900

    Chapter 3  pp.61-75
    Basis of development by Scaffolding
    
    index action

--- ruby_on_rails/data/railsbook/db/schema.rb
+++ ruby_on_rails/data/railsbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617125320) do
+ActiveRecord::Schema.define(version: 20150617144714) do
 
   create_table "books", force: true do |t|
     t.string   "isbn"
