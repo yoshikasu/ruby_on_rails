22e8d191c10735bd284a93479b15e6470f0cf004
yoshikazu HIRAI <hirai.yoshikazu@adways.net>
2015-06-26 10:23:45 +0900

    Chapter 4 pp.109-115
    Basis of development by view

--- ruby_on_rails/data/railsbook/app/controllers/view_controller.rb
+++ ruby_on_rails/data/railsbook/app/controllers/view_controller.rb
@@ -1,4 +1,5 @@
 class ViewController < ApplicationController
+
   def form_tag
     @book = Book.new
   end
@@ -7,7 +8,34 @@ class ViewController < ApplicationController
     @book = Book.new
   end
 
+  def field
+    @book = Book.new
+  end
+
+  def html5
+    @book = Book.new
+  end
+
   def select
     @book = Book.new(publish: '技術評論社')
+  end
+
+  def col_select
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def group_select
+    @review = Review.new
+    @authors = Author.all
+  end
+
+  def group_select2
+    @review = Review.new
+    @authors = Author.all
+  end
+
+  def fields
+    @user = User.find(1)
   end
 end
