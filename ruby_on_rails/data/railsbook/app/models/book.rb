70615fc920a0eeacb2e38095fc463515ffdeec93
yoshikazu HIRAI <hirai.yoshikazu@adways.net>
2015-08-05 00:27:02 +0900

    Chapter 5 pp.250-263

--- ruby_on_rails/data/railsbook/app/models/book.rb
+++ ruby_on_rails/data/railsbook/app/models/book.rb
@@ -19,8 +19,8 @@ class Book < ActiveRecord::Base
     uniqueness: { allow_blank: true },
     length: { is: 17 , allow_blank: true },
     format: { with: /\A[0-9]{3}-[0-9]{1}-[0-9]{3,5}-[0-9]{4}-[0-9X]{1}\z/, allow_blank: true }
-    isbn: true
-    isbn: { allow_old: true }
+    #isbn: true
+    #isbn: { allow_old: true }
 
 
   validates :isbn,
--- ruby_on_rails/data/railsbook/app/models/user.rb
+++ ruby_on_rails/data/railsbook/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  validates :agreement, acceptance: true { on: :create }
-  validates :email, confirmation: true
-  validates :email, presence: { if: '!dm.blank?' }
+  has_one :author
+  has_many :reviews
+  has_many :books, through: :reviews
+
+  #validates :agreement, acceptance: true { on: :create }
+  #validates :email, confirmation: true
+  #validates :email, presence: { if: '!dm.blank?' }
 end
