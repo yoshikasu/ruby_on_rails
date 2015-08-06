9f029f24208bdf58fefc298c0e292e0ab75949b1
yoshikazu HIRAI <hirai.yoshikazu@adways.net>
2015-08-07 00:26:33 +0900

    Chapter 5 pp.264-275

--- ruby_on_rails/data/railsbook/app/models/book.rb
+++ ruby_on_rails/data/railsbook/app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ActiveRecord::Base
   has_many :reviews
   has_and_belongs_to_many :authors
   has_many :users, through: :reviews
-  #has_many :memos, as: :memoable
+  has_many :memos, as: :memoable
 
 
 
